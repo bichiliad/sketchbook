77cf7d38619dd869643646fd182805a5db78297c
Salem <salemhilal@gmail.com>
2015-06-14 15:36:20 -0400

    Player looks kinda nice, added download link

--- downspout/js/app.js
+++ downspout/js/app.js
@@ -1,4 +1,15 @@
 angular.module('downspout', ['plangular', 'ngAnimate', 'ui.unique'])
     .config(function(plangularConfigProvider) {
         plangularConfigProvider.clientId = '2b7f29617f693e9913cb25ca16703473'
+    })
+    .directive('imageonload', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                element.bind('load', function() {
+                    //call the function that was passed
+                    scope.$apply(attrs.imageonload);
+                });
+            }
+        };
     });
