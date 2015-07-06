b05b5839f8d1dbbbd6d69ec27d2641d16faf20ef
Salem <salemhilal@gmail.com>
2015-07-06 13:27:12 -0400

    More responsive work tiles

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,40 +1,67 @@
-var timeout = -1;
-var resizeFont = function() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function() {
-        var $content = $('#content'),
-            target = $(window).width(),
-            current = $('.measure').width(),
-            fontSize = parseInt($content.css('font-size'));
+// Keep things from happening more often than they should.
+var throttle = function(fn, throttle) {
+    var timeout = -1;
 
-        if (current > target * .85) {
-            while (current > target * .85) {
-                fontSize--;
-                $content.css('font-size', fontSize + 'px');
-                current = $('.measure').width();
-            }
-        } else if (current < target * .75) {
-            while (current < target * .75) {
-                fontSize++;
-                $content.css('font-size', fontSize + 'px');
-                current = $('.measure').width();
-                console.log('unmeasure', current);
-            }
-        }
-    }, 100);
-
+    return function() {
+        clearTimeout(timeout);
+        timeout = setTimeout(fn, throttle);
+    }
 }
 
+// Resize the font to fit the screen.
+var resizeFont = function() {
+    var $content = $('.resize-me'),
+        $measure = $content.find('.measure');
 
-$(window).ready(function() {
+    var target = $('.container').first().width(),
+        current = $measure.width(),
+        fontSize = parseInt($content.css('font-size'));
 
-    resizeFont();
+    // Don't do anything for mobile.
+    if (target < 667) {
+        $content.css('font-size', 'inherit');
+        return;
+    }
+
+    if (current > target * .85) {
+        while (current > target * .85) {
+            fontSize--;
+            $content.css('font-size', fontSize + 'px');
+            current = $measure.width();
+        }
+    } else if (current < target * .75) {
+        while (current < target * .75) {
+            fontSize++;
+            $content.css('font-size', fontSize + 'px');
+            current = $measure.width();
+        }
+    }
+};
+
+
+// Get things started
+$(document).ready(function() {
+    // So we don't get that pop of red at the beginning
+    //$('body').css('background-color', '#E3454D');
+
+    // Fade in the title.
     setTimeout(function() {
-        $('.fade-in').animate({
-            'opacity': 1
-        }, 200);
+        resizeFont();
+        $('.fade-in').addClass('done');
     }, 300);
 
-    $(window).resize(resizeFont);
+    // Let people know they can scroll.
+    setTimeout(function() {
+        $('#hello').addClass('done');
+    }, 2000);
+
+    $(window)
+        .resize(throttle(function() {
+            resizeFont();
+        }, 80))
+        .scroll(function() {
+            $('#hello').addClass('done');
+            $(this).off('scroll');
+        });
 
 });
